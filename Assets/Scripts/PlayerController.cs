20919d1201471420658cb9f88ed8a7fbb30b4bb5
Si-Son! <epicdudeness677@gmail.com>
2019-03-09 13:36:42 +0100

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,28 +10,48 @@ public class PlayerController : CharacterController {
     bool buttonJump;
     Rigidbody2D rb;
     [SerializeField] GroundVariables groundVariables;
-    [System.Serializable]
-    class GroundVariables {
+    [SerializeField] SlapMechanics slapInfo;
+
+    [System.Serializable] class GroundVariables {
         public Vector2 groundPoint;
         public Vector2 groundRadius;
         public LayerMask groundMask;
+    }
+
+    [System.Serializable] class SlapMechanics {
+        public Slapdata normalSlap;
+        public Slapdata heavySlap;
+        public float midAirDashSpeed;
+
+        public RaycastHit2D pontentialTargetToBeSpanked;
     }
     void Awake () {
         rb = GetComponent<Rigidbody2D> ();
     }
     protected override void Update () {
         base.Update ();
-        buttonJump = Jump ();
-        Debug.Log (buttonJump);
 
     }
 
     bool Jump () => Input.GetButton (jumpButtonSrc);
+    RaycastHit2D SlapRay () => Physics2D.Raycast (transform.position, transform.right, 1);
     private void FixedUpdate () {
         Movement ();
     }
 
+    public override void Slap (Slapdata slapdata, GameObject slapOrigin) {
+        base.Slap (slapdata, slapOrigin);
+
+    }
+
     bool GroundCheck () => Physics2D.OverlapBox (new Vector2 (transform.position.x, transform.position.y) + groundVariables.groundPoint, groundVariables.groundRadius, 0, groundVariables.groundMask);
+
+    void OnNormalAttack () {
+        if (SlapRay ().collider.GetComponent<ISlappable> () == null) {
+            return;
+        }
+        Debug.Log ("Slapped someone!" + SlapRay ().collider.name);
+    }
     void Movement () {
         Vector2 direction = new Vector3 (joy1X, 0);
         rb.velocity = (direction.normalized * xAxisMovementSpeed) + new Vector2 (0, rb.velocity.y);
@@ -57,7 +77,13 @@ public class PlayerController : CharacterController {
         Gizmos.DrawWireCube (new Vector2 (transform.position.x, transform.position.y) + groundVariables.groundPoint, groundVariables.groundRadius);
     }
 
+    void GizmoDrawHitBox () {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay (transform.position, transform.right);
+    }
+
     private void OnDrawGizmosSelected () {
         GizmoDraw ();
+        GizmoDrawHitBox ();
     }
 }
