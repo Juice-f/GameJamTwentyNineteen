f28f6be0e61d714ab8145e7f029c6e5751dc4c5e
Juice-f <47383418+Juice-f@users.noreply.github.com>
2019-03-09 10:26:41 +0100

    Base character

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterController : MonoBehaviour, ISlappable
+{
+    public UnityEvent OnSlap;
+
+    public enum Player { one, two, three, four }
+    [SerializeField]
+    Player controlledByPlayer = Player.one;
+
+
+    public float joy1X, joy1Y;
+    void UpdateAxi()
+    {
+        joy1X = Input.GetAxisRaw(joy1XInputSrc);
+        joy1Y = Input.GetAxisRaw(joy1YInputSrc);
+    }
+
+    public string joy1XInputSrc, joy1YInputSrc, jumpButtonSrc, bigSlapButtonSrc, smolSlapButtonSrc, gimmickButtonSrc;
+
+
+    public Player ControlledByPlayer
+    {
+        get => controlledByPlayer;
+        set
+        {
+            controlledByPlayer = value;
+        }
+    }
+
+    public virtual void Slap(float slapForce, GameObject slapOrigin)
+    {
+        Debug.Log("I was slapped");
+        OnSlap.Invoke();
+    }
+
+
+}
---
+++ Assets/Scripts/ISlappable.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISlappable
+{
+    void Slap(float slapForce, GameObject slapOrigin);
+}
